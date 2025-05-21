49ae35797003739d787cd43d2f20446451582049
Sanshiya <sanshiyasayshi@gmail.com>
2025-05-21 17:41:27 +0100

    Streamlit version of ML Study Planner added

--- predict.py
+++ predict.py
@@ -1,35 +1,28 @@
 import joblib
 import pandas as pd
+import streamlit as st
 
-# Load model and encoders
 model = joblib.load("model/model.pkl")
 style_encoder, topic_encoder = joblib.load("model/encoders.pkl")
 
-# Example user input
 hours_per_day = 1
 days_per_week = 1
 preferred_style = "article"
 
-# Encode style
 style_encoded = style_encoder.transform([preferred_style])[0]
 
-# Prepare input
 X_new = pd.DataFrame([{
     "hours_per_day": hours_per_day,
     "days_per_week": days_per_week,
     "style_encoded": style_encoded
 }])
 
-# Predict
 predicted_index = model.predict(X_new)[0]
 predicted_topic = topic_encoder.inverse_transform([predicted_index])[0]
 
-print("🧠 Recommended Topic:", predicted_topic)
-
-# predict.py
-print("Model Prediction Input Data:", X_new)
-predicted_index = model.predict(X_new)[0]
-print("Model Predicted Index:", predicted_index)
-
-predicted_topic = topic_encoder.inverse_transform([predicted_index])[0]
-print("Model Predicted Topic:", predicted_topic)
+st.title("🧠 Recommended Topic")
+st.write(predicted_topic)
+st.subheader("Model Prediction Input Data")
+st.dataframe(X_new)
+st.write("Model Predicted Index:", predicted_index)
+st.write("Model Predicted Topic:", predicted_topic)
